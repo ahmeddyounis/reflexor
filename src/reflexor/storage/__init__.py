85f43304913f2c4fd107509948ae2b48bdd72b3e
ahmedyounis <ahmed.dusky@outlook.com>
2026-03-04 14:14:18 -0800

    Re-export idempotency port from storage

--- src/reflexor/storage/__init__.py
+++ src/reflexor/storage/__init__.py
@@ -1,5 +1,12 @@
 from __future__ import annotations
 
+from reflexor.storage.idempotency import (
+    DEFAULT_MAX_CACHED_OUTCOME_BYTES,
+    CachedOutcome,
+    IdempotencyLedger,
+    LedgerStatus,
+    OutcomeToCache,
+)
 from reflexor.storage.ports import (
     ApprovalRepo,
     EventRepo,
@@ -17,10 +24,15 @@ from reflexor.storage.uow import DatabaseSession, UnitOfWork
 
 __all__ = [
     "ApprovalRepo",
+    "CachedOutcome",
     "DatabaseSession",
+    "DEFAULT_MAX_CACHED_OUTCOME_BYTES",
     "EventRepo",
     "EventSuppressionRecord",
     "EventSuppressionRepo",
+    "IdempotencyLedger",
+    "LedgerStatus",
+    "OutcomeToCache",
     "RunPacketRepo",
     "RunRecord",
     "RunRepo",
