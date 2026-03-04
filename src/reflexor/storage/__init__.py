312c930aaeec452a220d405648f8c0d77ff4ee03
ahmedyounis <ahmed.dusky@outlook.com>
2026-03-04 11:59:07 -0800

    Standardize storage boundary exports

--- src/reflexor/storage/__init__.py
+++ src/reflexor/storage/__init__.py
@@ -3,23 +3,30 @@ from __future__ import annotations
 from reflexor.storage.ports import (
     ApprovalRepo,
     EventRepo,
+    EventSuppressionRecord,
+    EventSuppressionRepo,
     RunPacketRepo,
     RunRecord,
     RunRepo,
+    RunSummary,
     TaskRepo,
+    TaskSummary,
     ToolCallRepo,
 )
-from reflexor.storage.uow import UnitOfWork
-
-__all__ = ["UnitOfWork"]
+from reflexor.storage.uow import DatabaseSession, UnitOfWork
 
 __all__ = [
     "ApprovalRepo",
+    "DatabaseSession",
     "EventRepo",
+    "EventSuppressionRecord",
+    "EventSuppressionRepo",
     "RunPacketRepo",
     "RunRecord",
     "RunRepo",
+    "RunSummary",
     "TaskRepo",
+    "TaskSummary",
     "ToolCallRepo",
     "UnitOfWork",
 ]
